ae0dcd4a35ef3e962c238599b0010455b8db984e
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2013-09-10 19:44:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResourceFlow::Application.config.secret_token = '9def56a2b43592c359ccc3bc34bb7ce198d30874a220a0808824914d0302f9448e7930cb582e0f678f1a8466df7d2a1ae8f0d8b4537e7e01f352ff00cae4295e'
